e57511e9c020e8e0a98744924b7ac738aef9f610
Dorian Brown <mail@dorianbrown.dev>
2025-03-11 23:54:47 +0100

    Moved bluetooth back to own branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import '/utils/data.dart';
 import '/widgets/decks_overview.dart';
-import '/widgets/deck_scanner.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
