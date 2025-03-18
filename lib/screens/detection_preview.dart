0ca803873c81868c0bc18df361007cc9f0062786
Dorian Brown <mail@dorianbrown.dev>
2025-03-18 12:41:49 +0100

    Updated detection preview screen

--- lib/screens/detection_preview.dart
+++ lib/screens/detection_preview.dart
@@ -1,9 +1,8 @@
 import 'dart:async';
-import 'dart:isolate';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart' hide Card;
 import 'package:image/image.dart' as img;
-import 'package:fuzzywuzzy/fuzzywuzzy.dart';
 
 import 'deck_viewer.dart';
 import '/utils/data.dart';
@@ -11,38 +10,99 @@ import '/utils/models.dart';
 
 DeckStorage _deckStorage = DeckStorage();
 
-class DetectionPreviewScreen extends StatelessWidget {
+class DetectionPreviewScreen extends StatefulWidget {
   final img.Image image;
-  final List<Card> matchedCards;
+  final List<Detection> detections;
 
   const DetectionPreviewScreen(
-    {super.key, required this.image, required this.matchedCards});
+      {super.key, required this.image, required this.detections});
+
+  @override
+  _detectionPreviewState createState() => _detectionPreviewState(image, detections);
+}
+
+class _detectionPreviewState extends State<DetectionPreviewScreen> {
+  final img.Image image;
+  final List<Detection> detections;
+  _detectionPreviewState(this.image, this.detections);
+
+  late Uint8List imagePng;
+  List<Card> allCards = [];
+  ScrollController _scrollController = new ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _deckStorage.getAllCards().then((value) => setState(() {allCards = value;}));
+    imagePng = img.encodePng(image);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Detection Preview')),
-      body: Center(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            double aspectRatio = image.width / image.height;
-            double translationY = 0.5*(constraints.maxHeight - (constraints.maxWidth / aspectRatio));
-            double minScale = constraints.maxWidth / image.width;
-            final scaleMatrix = Matrix4.identity()..scale(minScale);
-            scaleMatrix.setEntry(1, 3, translationY);
-            final viewTransformationController = TransformationController(scaleMatrix);
-            return InteractiveViewer(
-              constrained: false,
-              clipBehavior: Clip.none,
-              minScale: minScale,
-              maxScale: 1,
-              boundaryMargin: const EdgeInsets.all(double.infinity),
-              transformationController: viewTransformationController,
-              child: Image.memory(img.encodePng(image))
-            );
-          }
-        )
+      body: ListView.separated(
+        controller: _scrollController,
+        separatorBuilder: (context, index) => Divider(indent: 10, endIndent: 10,),
+        padding: EdgeInsets.all(5),
+        itemCount: detections.length,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            confirmDismiss: confirmDeletion,
+            key: UniqueKey(),
+            background: Container(color: Colors.red,),
+            onDismissed: (direction) {
+              setState(() {
+                detections.removeAt(index);
+              });
+            },
+            child: Row(
+              spacing: 15,
+              children: [
+                Text("${index + 1}"),
+                Expanded(
+                  flex: 1,
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(maxHeight: 30),
+                      child: detections[index].textImage != null
+                          ? Image.memory(img.encodePng(detections[index].textImage!))
+                          : Text(""),
+                    )
+                ),
+                if (kDebugMode)
+                  Expanded(
+                    flex: 1,
+                    child: Text(detections[index].ocrText, style: TextStyle(height: 1.1),)
+                  ),
+                Expanded(
+                  flex: 1,
+                  child: Autocomplete(
+                    initialValue: TextEditingValue(text: detections[index].card.name),
+                    optionsViewOpenDirection: OptionsViewOpenDirection.up,
+                    optionsBuilder: (val) {
+                      if (val.text == "") {
+                        return const Iterable<String>.empty();
+                      }
+                      return allCards
+                          .where((el) => el.name.toLowerCase().contains(val.text.toLowerCase()))
+                          .map((el) => el.name)
+                          .toList();
+                    },
+                    onSelected: (option) {
+                      Card newCard = allCards.firstWhere((x) => x.name == option);
+                      setState(() {
+                        detections[index].card = newCard;
+                      });
+                      debugPrint(detections.map((x) => x.card.name).toList().toString());
+                    },
+                  )
+                ),
+              ],
+            )
+          );
+        }
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endContained,
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () async {
           Navigator.of(context).push(
@@ -51,7 +111,7 @@ class DetectionPreviewScreen extends StatelessWidget {
                   Center(child: CircularProgressIndicator()),
             ),
           );
-          final deckId = await createDeckAndSave(matchedCards);
+          final deckId = await createDeckAndSave(detections.map((x) => x.card).toList());
           debugPrint("Deck saved with id: $deckId");
           Navigator.of(context).pushAndRemoveUntil(
             MaterialPageRoute(
@@ -61,12 +121,93 @@ class DetectionPreviewScreen extends StatelessWidget {
           );
         },
         label: Text("Save Deck"),
-        icon: Icon(Icons.add)),
+        icon: Icon(Icons.save)
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () async {
+                detections.add(Detection(
+                  card: allCards.firstWhere((x) => x.name == "Fblthp, the Lost"),
+                  ocrText: ""
+                ));
+                setState(() {});
+                _scrollController
+                    .animateTo(
+                    _scrollController.position.extentTotal,
+                      duration: const Duration(milliseconds: 500
+                    ),
+                  curve: Curves.easeOut
+                );
+              },
+              icon: Icon(Icons.add)
+            ),
+            IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return Dialog(
+                      child: createInteractiveViewer(image),
+                    );
+                  }
+                );
+              },
+              icon: Icon(Icons.image)
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  LayoutBuilder createInteractiveViewer(img.Image image) {
+    return LayoutBuilder(
+        builder: (context, constraints) {
+          double aspectRatio = image.width / image.height;
+          double translationY = 0.5*(constraints.maxHeight - (constraints.maxWidth / aspectRatio));
+          double minScale = constraints.maxWidth / image.width;
+          final scaleMatrix = Matrix4.identity()..scale(minScale);
+          scaleMatrix.setEntry(1, 3, translationY);
+          final viewTransformationController = TransformationController(scaleMatrix);
+          return InteractiveViewer(
+              constrained: false,
+              clipBehavior: Clip.none,
+              minScale: minScale,
+              maxScale: 1,
+              boundaryMargin: const EdgeInsets.all(double.infinity),
+              transformationController: viewTransformationController,
+              child: Image.memory(imagePng)
+          );
+        }
     );
   }
 
   Future<int> createDeckAndSave(List<Card> matchedCards) async {
     final DateTime dateTime = DateTime.now();
     return await _deckStorage.saveNewDeck(dateTime, matchedCards);
+  }
+
+  Future<bool> confirmDeletion(direction) async {
+    return await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text("Confirm"),
+        content: const Text("Are you sure you wish to delete this item?"),
+        actions: <Widget>[
+          TextButton(
+              onPressed: () => Navigator.of(context).pop(true),
+              child: const Text("DELETE")
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text("CANCEL"),
+          ),
+        ],
+      );
+    },
+    );
   }
 }
