a56c2c8d327d417d955dc0b8ce7bdecc0edd10f8
Dorian Brown <mail@dorianbrown.dev>
2025-05-26 09:45:59 +0200

    Revert "First working bit for statistics"
    This reverts commit d44e68c68dbc2445ff0e63c7889a4b2f15efc2dc.

--- lib/widgets/card_statistics.dart
+++ lib/widgets/card_statistics.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart' hide Card, Orientation;
-import 'package:sqflite/sqflite.dart';
 
 import '/utils/data.dart';
 import '/utils/models.dart';
-import '/utils/statistics.dart';
 
 class CardStatistics extends StatefulWidget {
   final Set? set;
@@ -20,20 +18,21 @@ class CardStatisticsState extends State<CardStatistics> {
   final Set? set;
   final Cube? cube;
   CardStatisticsState(this.set, this.cube);
-  late StatisticsRetriever statsRetriever;
-  Future? futureCardWinRates;
+
+  late Future<List<Deck>> decksFuture;
+  late Future<List<Set>> setsFuture;
+  late Future<List<Cube>> cubesFuture;
+  late Future buildFuture;
+  late DeckStorage _deckStorage;
 
   @override
   void initState() {
     super.initState();
-    statsRetriever = StatisticsRetriever();
-    statsRetriever.init().then((_) {
-      futureCardWinRates = statsRetriever.getCardWinRates(
-          cubeId: cube?.cubecobraId, setId: set?.code);
-      futureCardWinRates!.then((_) {
-        setState(() {});
-      });
-    });
+    // Retrieving deck/set/cube data
+    _deckStorage = DeckStorage();
+    decksFuture = _deckStorage.getAllDecks();
+    setsFuture = _deckStorage.getAllSets();
+    cubesFuture = _deckStorage.getAllCubes();
   }
 
   @override
@@ -53,41 +52,26 @@ class CardStatisticsState extends State<CardStatistics> {
         title: Text(name),
       ),
       body: FutureBuilder(
-          future: futureCardWinRates,
+          future: Future.wait([decksFuture, setsFuture, cubesFuture]),
           builder: (context, snapshot) {
             Widget widget;
             if (snapshot.connectionState != ConnectionState.done || snapshot.data == null) {
               widget = Center(child: CircularProgressIndicator());
             } else {
+              // Getting state
+              final decks = snapshot.data![0] as List<Deck>;
+              final sets = snapshot.data![1] as List<Set>;
+              final cubes = snapshot.data![2] as List<Cube>;
 
-              // Create card win rates
-              List cardWinRates = snapshot.data;
-              final columns = ["Name", "# Decks", "Win Rate"].map((el) =>
-                DataColumn(
-                  label: Expanded(
-                    child: Text(
-                      el,
-                      style: TextStyle(fontStyle: FontStyle.italic),
-                      overflow: TextOverflow.ellipsis,
-                    )
-                  )
-                )
-              ).toList();
+              List<Deck> statsDecks = (set != null)
+                  ? decks.where((deck) => deck.setId != null)
+                    .where((deck) => deck.setId == set!.code)
+                    .toList()
+                  : decks.where((deck) => deck.cubecobraId != null)
+                    .where((deck) => deck.cubecobraId == cube!.cubecobraId)
+                    .toList();
 
-              final rows = cardWinRates.map((el) => DataRow(
-                cells: [
-                  DataCell(Text(el['name'].toString())),
-                  DataCell(Text(el['num_decks'].toString())),
-                  DataCell(Text(el['winrate'].toString())),
-                ]
-              )).toList();
-
-              widget = SingleChildScrollView(
-                child: DataTable(
-                    columns: columns,
-                    rows: rows
-                ),
-              );
+              widget = Text("Placeholder");
 
             }
             return AnimatedSwitcher(
