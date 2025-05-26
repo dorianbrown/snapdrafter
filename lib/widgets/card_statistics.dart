fc39f1fb243840dd8fe1a4f29e8fc4aef2cf6271
Dorian Brown <mail@dorianbrown.dev>
2025-05-26 09:45:59 +0200

    Revert "Made most-played cards table usable"
    This reverts commit def8fc8f1e3a5fcd65fdcd5165c429a71dd0daf8.

--- lib/widgets/card_statistics.dart
+++ lib/widgets/card_statistics.dart
@@ -82,11 +82,11 @@ class CardStatisticsState extends State<CardStatistics> {
                 ]
               )).toList();
 
-              widget = PaginatedDataTable(
-                rowsPerPage: 10,
-                showFirstLastButtons: true,
-                columns: columns,
-                source: ListDataSource(cardWinRates),
+              widget = SingleChildScrollView(
+                child: DataTable(
+                    columns: columns,
+                    rows: rows
+                ),
               );
 
             }
@@ -98,32 +98,4 @@ class CardStatisticsState extends State<CardStatistics> {
       )
     );
   }
-}
-
-class ListDataSource extends DataTableSource {
-  List<dynamic> listSource;
-  ListDataSource(this.listSource);
-
-  @override
-  int get rowCount => listSource!.length;
-
-  @override
-  DataRow? getRow(int index) {
-
-    List<dynamic> rowList = listSource![index].row;
-
-    List<DataCell> cells = rowList
-        .map((val) => DataCell(Text(val.toString())))
-        .toList();
-
-    return DataRow(
-      cells: cells
-    );
-  }
-
-  @override
-  bool get isRowCountApproximate => false;
-
-  @override
-  int get selectedRowCount => 0;
 }
