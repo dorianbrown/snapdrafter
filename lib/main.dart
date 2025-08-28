bb58929c68de861b1ecd26460e2e99a9638ebc76
Dorian Brown <mail@dorianbrown.dev>
2025-08-28 14:06:30 +0200

    feat: add image sharing intent handling and processing screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_sharing_intent/model/sharing_file.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_sharing_intent/flutter_sharing_intent.dart';
 
 import '/data/database/database_helper.dart';
 import '/screens/decks_overview.dart';
+import '/screens/image_processing_screen.dart';
 import '/utils/theme_notifier.dart';
 import '/utils/themes.dart';
 
@@ -39,7 +42,50 @@ Future<void> main() async {
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => MainAppState();
+}
+
+class MainAppState extends State<MainApp> {
+  late StreamSubscription _intentDataStreamSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _intentDataStreamSubscription = FlutterSharingIntent.instance.getMediaStream()
+      .listen((List<SharedFile> val) {
+        if (val.isNotEmpty) {
+          final String sharedFile = val[0].value!;
+          addShareIntentCallback(sharedFile);
+        }
+    });
+
+    FlutterSharingIntent.instance.getInitialSharing().then((List<SharedFile> val) {
+      if (val.isNotEmpty){
+        final String sharedFile = val[0].value!;
+        addShareIntentCallback(sharedFile);
+      }
+    });
+  }
+
+  void addShareIntentCallback(String imagePath) async {
+    // Wait for the app to be fully built before navigating
+    await Future.delayed(Duration(milliseconds: 100));
+
+    if (mounted) {
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (context) => deckImageProcessing(filePath: imagePath),
+        ),
+            (route) => false, // Remove all previous routes
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<ThemeNotifier>(
